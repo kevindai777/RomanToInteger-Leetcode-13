670ab25899f610cbca91fcc503d6fe09a08bca9e
Kevin Dai <kevin.dai.2451@gmail.com>
2020-09-14 04:16:15 -0400

    added java solution

---
+++ romantointeger.java
@@ -0,0 +1,32 @@
+//Java Solution
+
+class Solution {
+    public int romanToInt(String s) {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("I", 1);
+        map.put("V", 5);
+        map.put("X", 10);
+        map.put("L", 50);
+        map.put("C", 100);
+        map.put("D", 500);
+        map.put("M", 1000);
+        map.put("IV", 4);
+        map.put("IX", 9);
+        map.put("XL", 40);
+        map.put("XC", 90);
+        map.put("CD", 400);
+        map.put("CM", 900);
+        
+        int total = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (i < s.length() - 1 && map.containsKey(s.substring(i, i + 2))) {
+                total += map.get(s.substring(i, i + 2));
+                i++;    
+            } else {
+                total += map.get(s.substring(i, i + 1));
+            }
+        }
+        
+        return total;
+    }
+}
